d65ff96605daa09edbb1580f711660235d0ce206
jonnadaindhu <119721592+jonnadaindhu@users.noreply.github.com>
2023-06-17 04:34:15 +0530

    #include<stdio.h> int main() { int n,i,j,z; scanf("%d",&n); for(i=0;i<n;i++) {     for(j=n;j>i+1;j--)     {         printf(" ");     }     for(z=1;z<=2*i+1;z++)     printf("%d",i+1);     printf(" ");  } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,19 +1,17 @@
-
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
+int n,i,j,z;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+    for(j=n;j>i+1;j--)
     {
-        for(j=1;j<=n;j++)
-        {
-            if(i==j)
-            printf("0");
-            if(j<=n-1)
-            printf("x");
-        }
-        printf("
-");
+        printf(" ");
     }
+    for(z=1;z<=2*i+1;z++)
+    printf("%d",i+1);
+    printf("
+");
+ }
 }
